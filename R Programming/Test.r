ab812c2eac7b7c3c2a8760201a5033dd0ce8b9ce
Philip John Pereira <pc212002@gmail.com>
2025-08-12 08:34:10 +0530

    Changes in first r programming file

--- R Programming/Test.r
+++ R Programming/Test.r
@@ -1,2 +1,13 @@
 message <- "Hello, World"
 print(message)
+
+# Sample data
+data <- c(10, 20, 30, 40, 50)
+
+# Mean calculation
+mean_value <- mean(data)
+print(paste("Mean:", mean_value))
+
+# Standard deviation calculation
+sd_value <- sd(data)
+print(paste("Standard Deviation:", sd_value))
