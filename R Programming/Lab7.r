241f1c51ee2584a0854a8d80b074fcba0b310e56
Philip John Pereira <pc212002@gmail.com>
2025-09-11 10:24:18 +0530

    changes

--- R Programming/Lab7.r
+++ R Programming/Lab7.r
@@ -3,7 +3,7 @@ strings <- c("R", "Programming", "Language")
 # Find length of each string
 lengths <- nchar(strings)
 # Display results
-#data.frame(String = strings, Length = lengths)
+#data . frame (String = strings, Length = lengths)
 result <- data.frame(String = strings, Length = lengths)
 # Print the result
 print(result)
