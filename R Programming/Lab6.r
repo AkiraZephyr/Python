014ff7a56d77e1f8c7575dad143d27f22d1013d4
Philip John Pereira <pc212002@gmail.com>
2025-09-11 10:23:39 +0530

    changes

--- R Programming/Lab6.r
+++ R Programming/Lab6.r
@@ -1,16 +1,15 @@
-numbers <- c(2, 3, 4, 5, 10, 13, 17, 20) 
- 
-check_prime <- function(n) 
-{   if (n <= 1) return(FALSE) 
-   
- for (i in 2:sqrt(n)) 
-  {     if (n %% i == 0) return(FALSE)   } 
-   
-return(TRUE) 
-} 
- 
-prime_status <- sapply (numbers, check_prime) 
- 
-result <- data.frame(Number = numbers, IsPrime = prime_status) 
- 
-print (result)
+numbers <- c(2, 3, 4, 5, 10, 13, 17, 20)
+
+check_prime <- function(n) {
+  if (n <= 1) return(FALSE)
+  for (i in 2:floor(sqrt(n))) {
+    if (n %% i == 0) return(FALSE)
+  }
+  TRUE
+}
+
+prime_status <- sapply(numbers, check_prime)
+
+result <- data.frame(Number = numbers, IsPrime = prime_status)
+
+print(result)
