0a03fbc30329e87168d9c3e7787cedbe4b841d85
Philip John Pereira <pc212002@gmail.com>
2025-10-09 10:04:07 +0530

    Fixes

--- R Programming/Lab8.r
+++ R Programming/Lab8.r
@@ -3,9 +3,9 @@ search_char <- "g"
 # Find positions using gregexpr()
 positions <- gregexpr(search_char, main_string)[[1]]
 if (positions[1] != -1) {
-      cat(sprintf("Character '%s' found at positions: %s\n",
-                    search_char,
-                    paste(positions, collapse = ", ")))
+  cat(sprintf("Character '%s' found at positions: %s\n",
+              search_char,
+              paste(positions, collapse = ", ")))
 } else {
-      cat(sprintf("Character '%s' not found.\n", search_char))
+  cat(sprintf("Character '%s' not found.\n", search_char))
 }
